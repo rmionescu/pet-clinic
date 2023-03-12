e669f20b8cda38622762a97a104090b949f140c9
rmionescu <rmionescu@yahoo.com>
2023-03-12 16:47:08 +0200

    Added Owners Index Page and Controllers. closes #12

---
+++ pet-clinic-web/src/main/java/emiione/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package emiione.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+	@GetMapping({ "", "/", "/index", "/index/html" })
+	public String listOwners() {
+		return "/owners/index";
+	}
+
+}
