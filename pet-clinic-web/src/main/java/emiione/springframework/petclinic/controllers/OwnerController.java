aba5c715c13e5fc6a7a8f19bd4b6c4522259778a
rmionescu <rmionescu@yahoo.com>
2023-03-15 12:09:55 +0200

    Added Owner List and updated controller. closes #15

--- pet-clinic-web/src/main/java/emiione/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/emiione/springframework/petclinic/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package emiione.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import emiione.springframework.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
 
 	@GetMapping({ "", "/", "/index", "/index/html" })
-	public String listOwners() {
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "/owners/index";
 	}
 
