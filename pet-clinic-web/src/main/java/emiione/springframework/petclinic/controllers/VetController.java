b632f2cab15e6953e7e5ac48477a8a2c740e9c78
rmionescu <rmionescu@yahoo.com>
2023-03-15 12:30:34 +0200

    Added Vet List and updated Controller. closes #14

--- pet-clinic-web/src/main/java/emiione/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/emiione/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package emiione.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import emiione.springframework.petclinic.services.VetService;
 
 @Controller
 public class VetController {
-	
-	@GetMapping({"vets", "vets/index", "vets/index.html"})
-	public String listVets() {
+
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+	@GetMapping({ "vets", "vets/index", "vets/index.html" })
+	public String listVets(Model model) {
+
+		model.addAttribute("vets", vetService.findAll());
+
 		return "vets/index";
 	}
 
